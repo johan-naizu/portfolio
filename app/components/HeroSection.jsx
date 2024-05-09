0703504c75a81b846b5499a57d71ae84e5020aaa
Johan Naizu <68628917+johan-naizu@users.noreply.github.com>
2024-05-10 03:07:00 +0530

    Update

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -43,7 +43,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/"
+              href="https://cdn.jonaz.tech/resume"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary to-secondary hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-bg hover:bg-[#295564] rounded-full px-5 py-2">
