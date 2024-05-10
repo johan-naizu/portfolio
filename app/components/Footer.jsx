ca2a85ac74b3bd9831bfc1ebe0c56f7ad6194dad
Johan Naizu <68628917+johan-naizu@users.noreply.github.com>
2024-05-10 23:53:17 +0530

    update

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="footer bg-bg border z-10 border-t-[#A2B8BE] border-l-transparent border-b-transparent border-r-transparent text-white">
-      <div className="container p-10 flex justify-between">
+      <div className="container p-10 flex justify-between mx-auto">
         <Link
           href={"#hero"}
           scroll={true}
