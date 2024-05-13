1de22c29552f875f7c67c95c3d33924d8edab4dd
Johan Naizu <68628917+johan-naizu@users.noreply.github.com>
2024-05-13 23:57:44 +0530

    UPDATE

--- app/components/MenuOverlay.jsx
+++ app/components/MenuOverlay.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import NavLink from "./NavLink";
 const MenuOverlay = ({ links }) => {
   return (
-    <ul className="flex flex-col py-4 text-xl h-screen justify-center items-center border border-transparent bg-bg bg-opacity-80">
+    <ul className="flex flex-col py-4 text-xl h-screen justify-center items-center border border-transparent bg-bg bg-opacity-80 md:hidden">
       {links.map((link, index) => (
         <li key={index}>
           <NavLink href={link.href} title={link.title} />
