a3b6c8f2a51c1a808a9b6991ea2881b348f96be7
Johan Naizu <68628917+johan-naizu@users.noreply.github.com>
2024-05-17 12:23:04 +0530

    transition

--- app/components/Loading.jsx
+++ app/components/Loading.jsx
@@ -1,17 +1,27 @@
 "use client";
 import Lottie from "react-lottie-player";
 import lottieJson from "/public/loading.json";
+import { motion, AnimatePresence } from "framer-motion";
 
-const Loading = () => {
+const Loading = ({ loading }) => {
   return (
-    <div className="h-dvh w-dvw flex justify-center items-center bg-bg z-50 fixed">
-      <Lottie
-        className="w-1/2 h-1/2 md:w-2/6 md:h-2/6"
-        loop
-        animationData={lottieJson}
-        play
-      />
-    </div>
+    <AnimatePresence>
+      {loading && (
+        <motion.div
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.25 }}
+          className="h-dvh w-dvw flex justify-center items-center bg-bg z-50 fixed"
+        >
+          <Lottie
+            className="w-1/2 h-1/2 md:w-2/6 md:h-2/6"
+            loop
+            animationData={lottieJson}
+            play
+          />
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
--- app/page.js
+++ app/page.js
@@ -31,7 +31,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col bg-bg">
-      {loading && <Loading />}
+      <Loading loading={loading} />
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
